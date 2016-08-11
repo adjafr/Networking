44d2140b5fdafd1a0c24e17065bcce22d13716c1
Adam Fraser <AJFraser10@GMail.com>
2016-08-11 10:04:19 -0500

    Initial import of socket-assignment

--- src/com/cooksys/client/Client.java
+++ src/com/cooksys/client/Client.java
@@ -1,12 +1,32 @@
 package com.cooksys.client;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.Socket;
+
 public class Client
 {
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
+		try (
+				Socket s = new Socket("10.1.1.143", 12345); //We now have a connection to the server.
+				OutputStream out = s.getOutputStream();
+				Writer base = new OutputStreamWriter(out);
+				Writer w = new BufferedWriter(base);
+				) {
+				
+				w.write("Adam Fraser");;
+				
+				//w.close();
+				w.flush();
+				
+			} catch (IOException e) {
+				e.printStackTrace();
 
 	}
-
+	}
 }
